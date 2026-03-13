3e6ef773b94fc13d755d813fb38b951b29ca7da0
PARKHYEONMIN <hmp9511@naver.com>
2026-03-13 12:22:15 +0900

    feat:settings 소스 구조화

---
+++ config/settings.py
@@ -0,0 +1,45 @@
+import sqlite3
+from pathlib import Path
+from typing import Literal
+from pydantic import Field, DirectoryPath, FilePath
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    ENV: Literal["local", "dev", "prod"] = Field(default="local")
+    OPEN_API_KEY: str = Field(..., alias="OPEN_API_KEY")
+    OCR_THRESHOLD: float = Field(default=0.8, ge=0.0, le=1.0)
+
+    BASE_DIR: Path = Path(__file__).resolve().parent.parent
+    DB_PATH: Path = Path("./data/finance_data.db")
+
+    DATA_RAW_DIR: DirectoryPath = Field(default=BASE_DIR / "logs")
+
+    model_config = SettingsConfigDict(
+        env_file=".env", env_file_encoding="utf-8", extra="ignore"
+    )
+
+
+config = Settings()
+
+
+def init_db():
+    config.DB_PATH.parent.mkdir(parents=True, exist_ok=True)
+    with sqlite3.connect(config.DB_PATH) as conn:
+        cursor = conn.cursor()
+        cursor.execute(
+            """
+                CREATE TABLE IF NOT EXISTS market_logs(
+                           id INTEGER PRIMARY KEY AUTOINCREMENT,
+                           timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+                           sentiment_score REAL,
+                           raw_data TEXT
+                           )
+                        """
+        )
+        conn.commit()
+        print(f"[시스템] DB 준비완료 : {self.DB_PATH}")
+
+
+if __name__ == "__main__":
+    init_db()
